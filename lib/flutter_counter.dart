05dad757b3eef43d19534b36dce62e2ecfb945c5
M Afiq <m.afiq@webcada.com>
2020-05-08 07:51:10 +0800

    change class name

--- lib/flutter_counter.dart
+++ lib/flutter_counter.dart
@@ -3,9 +3,9 @@ library flutter_counter;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(CounterWidget());
 
-class MyApp extends StatelessWidget {
+class CounterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<Counter>( // <=== PROVIDER
